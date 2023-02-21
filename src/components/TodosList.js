e17ed5faeab2ed18494a9e4b03d8b0843f31812f
teitonny <tonnytei4@gmail.com>
2023-02-21 20:04:37 +0300

    added checkbox function in the to-do list website

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
 
-const TodosList = ({ todosProps }) => {
+const TodosList = ({ todosProps, setTodos}) => {
+  
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+    prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    })
+  );
+  };
     
   return (
     <ul>
       {todosProps.map((todo) => (
-        <li>{todo.title}</li>
+        <li>
+          <input type="checkbox" checked = {todo.completed} onChange={() => handleChange(todo.id)} />
+          {todo.title}
+        </li>
       ))}
     </ul>
   );
--- src/components/todosLogic.js
+++ src/components/todosLogic.js
@@ -1,29 +1,30 @@
 /* eslint-disable */
+import { useState } from "react";
 import InputTodo from "./InputTodos";
 import TodosList from "./TodosList";
 
 const TodosLogic = () => {
-    const todos = [
-        {
-          id: 1,
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: 2,
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ];
+  const [todos, setTodos] = useState( [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ])
     return (
         <div>
             <InputTodo />
-            <TodosList todosProps={todos} />
+            <TodosList todosProps={todos} setTodos = {setTodos}/>
       </div>
     )
   }
